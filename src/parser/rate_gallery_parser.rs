24b05ab9f547c91f44a967c3bf874e3591e9d1f0
lauglam <lauglam@outlook.com>
2023-02-06 19:24:40 +0800

    feat(rate_gallery_parser): create rate gallery parser

--- src/parser/rate_gallery_parser.rs
+++ src/parser/rate_gallery_parser.rs
@@ -1,1 +1,19 @@
- 
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct RateGallery {
+    #[serde(alias = "rating_avg")]
+    pub rating: f32,
+    #[serde(alias = "rating_cnt")]
+    pub rating_count: i32,
+}
+
+impl RateGallery {
+    pub fn parse(json: &str) -> Result<RateGallery, String> {
+        if let Ok(rate_gallery) = serde_json::from_str::<RateGallery>(json) {
+            Ok(rate_gallery)
+        } else {
+            Err(String::from("Parses rate gallery fail."))
+        }
+    }
+}
