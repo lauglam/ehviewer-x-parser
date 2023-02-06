1a86124742e4892b099233e685eee3779b957115
lauglam <lauglam@outlook.com>
2023-02-06 13:12:58 +0800

    feat(vote_tag_parser): create vote tag parser

--- src/parser/vote_tag_parser.rs
+++ src/parser/vote_tag_parser.rs
@@ -1,1 +1,21 @@
- 
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct VoteTag {
+    pub error: String,
+}
+
+/// ```json
+/// {
+///     "error": "The tag \"neko\" is not allowed. Use character:neko or artist:neko"
+/// }
+/// ```
+impl VoteTag {
+    pub fn parse(json: &str) -> Result<VoteTag, String> {
+        if let Ok(vote_tag) = serde_json::from_str::<VoteTag>(json) {
+            Ok(vote_tag)
+        } else {
+            Err(String::from("Parses vote tag fail."))
+        }
+    }
+}
