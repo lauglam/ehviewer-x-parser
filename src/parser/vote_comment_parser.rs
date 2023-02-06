ee0e66944eebd951515aa97afa09d8a0d66a3fbc
lauglam <lauglam@outlook.com>
2023-02-06 13:31:54 +0800

    feat(vote_comment_parser): create vote comment parser

--- src/parser/vote_comment_parser.rs
+++ src/parser/vote_comment_parser.rs
@@ -1,1 +1,30 @@
- 
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct VoteComment {
+    #[serde(alias = r#"comment_id"#)]
+    pub id: u64,
+    #[serde(alias = r#"comment_score"#)]
+    pub score: i32,
+    #[serde(alias = r#"comment_vote"#)]
+    pub vote: u32,
+    // todo: need expect_vote?
+    // pub expect_vote: u32,
+}
+
+impl VoteComment {
+    /// ```json
+    /// {
+    ///     "comment_id": 1253922,
+    ///     "comment_score": -19,
+    ///     "comment_vote": 0
+    /// }
+    /// ```
+    pub fn parse(json: &str) -> Result<VoteComment, String> {
+        if let Ok(vote_comment) = serde_json::from_str(json) {
+            Ok(vote_comment)
+        } else {
+            Err(String::from("Parses vote comment fail."))
+        }
+    }
+}
